2eb1dac9c15bf07e74584407f0f4b7761e522ece
LonnyLiu <lonnyliu@126.com>
2016-02-19 18:14:19 +0800

    New Mysql Backup script

---
+++ bacpup_script/Mysql_DB_Backup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+# Author: "Edward.Liu"
+# Author-Email: lonnyliu@126.com
+
+
+"""
+Python Backup Mysql Database
+
+1.Start Local Backup Mysql Database
+2.Handle Local Backup Mysql Database(tar)
+3.Send Mysql Backup Mysql Database Files To Remote Server
+
+"""
